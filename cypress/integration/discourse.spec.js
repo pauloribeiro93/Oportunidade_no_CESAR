ae83b16255bef47878ceb9570ba2371c75aa3d03
pauloribeiro93 <pauloribeiro93@hotmail.com>
2021-07-22 14:18:47 -0300

    Arquivos de config para o report

--- cypress/integration/discourse.spec.js
+++ cypress/integration/discourse.spec.js
@@ -4,50 +4,37 @@
 
 beforeEach(() => {
     cy.intercept('**/message-bus/**').as('message')
-    cy.intercept('/latest.json?no_definitions=true&page=1').as('json')
-    cy.intercept('**/latest.json?**').as('json2')
+    cy.intercept('**/latest.json?**').as('json')
     cy.visit('http://www.discourse.org/');
-
-
 });
 
 context('Atividade de Automação 1', () => {
-    it('Imprimir o título de todos os tópicos fechados', () => {
-        cy.get('#main > :nth-child(2) > :nth-child(4) > a')
-            .invoke('removeAttr', 'target')
-            .click()
+    it.only('Imprimir o título de todos os tópicos fechados', () => {
 
+        cy.acessaValidaDemo()
 
         cy.window().scrollTo('bottom').as('scroll')
 
         cy.wait('@message')
         cy.wait('@json')
 
+        cy.window('@scroll')
 
-        cy.window().scrollTo('bottom')
-
-        let date = []
-        cy.get('tr[class*=closed]').find('.link-top-line').find('a')
-            .each(($el) => {
-                date.push($el.text())
-                cy.log(`Título de todos os tópicos fechados - ${date},`)
-            })
+        cy.titulosFechados()
     });
 
 
 
-    it('Imprimir quantidade de itens de cada categoria e dos que não possui categoria', () => {
-        cy.get('#main > :nth-child(2) > :nth-child(4) > a')
-            .invoke('removeAttr', 'target')
-            .click()
+    it.only('Imprimir quantidade de itens de cada categoria e dos que não possui categoria', () => {
 
+        cy.acessaValidaDemo()
 
         cy.window().scrollTo('bottom').as('scroll')
 
         cy.wait('@json')
         cy.wait('@message')
 
-        cy.window().scrollTo('bottom')
+        cy.window('@scroll')
 
         let date = []
         cy.get('[class="category-name"]')
@@ -55,45 +42,29 @@ context('Atividade de Automação 1', () => {
                 date.push($el.text())
             })
 
-        cy.arr(date)
-
+        cy.arrayCategorias(date)
         cy.get('tr.category-uncategorized').its('length').as('semCategoria')
     });
 
 
-    it(' Imprimir o título do tópico que contém o maior número de views;', () => {
-        cy.get('#main > :nth-child(2) > :nth-child(4) > a')
-            .invoke('removeAttr', 'target')
-            .click()
+    it.only(' Imprimir o título do tópico que contém o maior número de views;', () => {
 
+        cy.acessaValidaDemo()
 
         cy.window().scrollTo('bottom').as('scroll')
 
         cy.wait('@json')
         cy.wait('@message')
-        cy.wait('@json2')
 
-        cy.window().scrollTo('bottom')
+        cy.window('@scroll')
 
         let views = []
-
         cy.xpath('//td[@class="num views "]//span[@class="number"]')
             .each(($el) => {
                 views.push($el.text())
             })
-        cy.get(views).then((visu) => {
-            var max = Math.max(...visu)
-            cy.xpath(`//span[@title="this topic has been viewed ${max} times"]`).parents().then((valor) => {
-                cy.wrap(valor[1]).find("span[class='link-top-line']").invoke('text').then((texto) => {
-                    cy.writeFile('path/to/Maiorvisu.txt', `${texto}`)
-                    cy.log(`Tópico que contém o maior número de views - ${texto}`)
-                })
 
-            })
-
-
-        })
-
+        cy.maiorNumeroDeViews(views)
 
     });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
-Cypress.Commands.add('arr', (valor) => {
-    valor.reduce((todasCategorias, categoria) => {
+Cypress.Commands.add('arrayCategorias', (date) => {
+    date.reduce((todasCategorias, categoria) => {
         if (categoria in todasCategorias) {
             todasCategorias[categoria]++;
         } else {
@@ -12,6 +12,41 @@ Cypress.Commands.add('arr', (valor) => {
     }, {})
 
 })
+
+Cypress.Commands.add('titulosFechados', () => {
+    let date = []
+    cy.get('tr[class*=closed]').find('.link-top-line').find('a')
+        .each(($el) => {
+            date.push($el.text())
+            cy.log(`Título de todos os tópicos fechados - ${date},`)
+        })
+
+})
+
+Cypress.Commands.add('acessaValidaDemo', () => {
+    cy.get('#main > :nth-child(2) > :nth-child(4) > a')
+        .invoke('removeAttr', 'target')
+        .should('have.attr', 'href', 'https://try.discourse.org')
+        .click()
+
+})
+
+Cypress.Commands.add('maiorNumeroDeViews', (view) => {
+    cy.get(view).then((visu) => {
+        var max = Math.max(...visu)
+        cy.xpath(`//span[@title="this topic has been viewed ${max} times"]`).parents().then((valor) => {
+            cy.wrap(valor[1]).find("span[class='link-top-line']").invoke('text').then((texto) => {
+                cy.writeFile('path/to/Maiorvisu.txt', `${texto}`)
+                cy.log(`Tópico que contém o maior número de views - ${texto}`)
+            })
+
+        })
+
+
+    })
+
+})
+
 
 
 
