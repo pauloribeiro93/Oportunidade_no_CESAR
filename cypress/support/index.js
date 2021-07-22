5230f330779f8842300ee4373c0a5d9b3158d269
pauloribeiro93 <pauloribeiro93@hotmail.com>
2021-07-22 15:36:02 -0300

    ajuste gitignore e index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,7 +2,6 @@ import './commands'
 require('cypress-xpath')
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
+
     return false
   })
